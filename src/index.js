0446dcffdc0326a26cbc9f61f95068669a6e0fb4
塩漬けウミウシ <i@seaslug.moe>
2024-06-17 01:20:06 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,20 +1,14 @@
+import DOCS from '../help.html'
+ 
+
+
 addEventListener("fetch", (event) => {
   event.passThroughOnException();
   event.respondWith(handleRequest(event.request));
 });
 
 const dockerHub = "https://registry-1.docker.io";
-import DOCS from './help.html'
- 
-// return docs
-if (url.pathname === "/") {
-  return new Response(DOCS, {
-    status: 200,
-    headers: {
-      "content-type": "text/html"
-    }
-  });
-}
+
 const routes = {
   // production
   "docker.libcuda.so": dockerHub,
@@ -51,6 +45,15 @@ async function handleRequest(request) {
         status: 404,
       }
     );
+  }
+  // return docs
+  if (url.pathname === "/") {
+    return new Response(DOCS, {
+      status: 200,
+      headers: {
+        "content-type": "text/html"
+      }
+    });
   }
   const isDockerHub = upstream == dockerHub;
   const authorization = request.headers.get("Authorization");
